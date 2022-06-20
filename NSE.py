ff81f851b8094d54becb1de3b0fd2b57e1681cc3
PaulineMemo <103488901+PaulineMemo@users.noreply.github.com>
2022-06-20 14:58:51 +0300

    Update NSE.py

--- NSE.py
+++ NSE.py
@@ -23,23 +23,12 @@ end_date = st.sidebar.date_input("End date", datetime.date(2021, 1, 31))
 
 # Retrieving tickers data
 ticker_list = pd.read_csv('https://raw.githubusercontent.com/regan-mu/ADS-April-2022/main/Assignments/Assignment%201/data.csv')
-tickerSymbol = st.sidebar.selectbox('Stock ticker', ticker_list) # Select ticker symbol
-#tickerData = yf.Ticker(tickerSymbol) # Get ticker data
-tickerDf = tickerData.history(period='1d', start=start_date, end=end_date) #get the historical prices for this ticker
-
-# Ticker information
-string_logo = '<img src=%s>' % tickerData.info['logo_url']
-st.markdown(string_logo, unsafe_allow_html=True)
-
-#string_name = tickerData.info['longName']
-#st.header('**%s**' % string_name)
-
-#string_summary = tickerData.info['longBusinessSummary']
-#st.info(string_summary)
+tickerSymbol = st.sidebar.multiselect('ticker', ticker_list) # Select ticker symbol
+tickerDf = ticker_list.history(period='1d', start=start_date, end=end_date) #get the historical prices for this ticker
 
 # Ticker data
 st.header('**Ticker data**')
-st.write(tickerDf)
+st.write(ticker_list)
 
 # Bollinger bands
 st.header('**Bollinger Bands**')
